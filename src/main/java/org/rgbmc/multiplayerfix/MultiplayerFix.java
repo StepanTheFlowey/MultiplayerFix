13afbc17ebdc65657e8b6842ff397d3fd70929bc
flowey-pc <stepanmasirov@gmail.com>
2026-02-10 17:15:51 +0500

    minify mod to be even smaller

--- src/main/java/org/rgbmc/multiplayerfix/MultiplayerFix.java
+++ src/main/java/org/rgbmc/multiplayerfix/MultiplayerFix.java
@@ -2,12 +2,7 @@ package org.rgbmc.multiplayerfix;
 
 import net.fabricmc.api.ClientModInitializer;
 
-import java.util.logging.Logger;
-
 public class MultiplayerFix implements ClientModInitializer {
-    public static Logger logger = Logger.getLogger("MultiplayerFix");
     @Override
-    public void onInitializeClient() {
-        logger.info("MultiplayerFix by FlyProject");
-    }
+    public void onInitializeClient() {}
 }
