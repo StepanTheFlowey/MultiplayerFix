98dad8c30dd5eb86dde25a534349cf11e03c0b59
flowey-pc <stepanmasirov@gmail.com>
2026-02-15 22:27:45 +0500

    jvm options

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 git clean -xdf
 
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
-export JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:-TieredCompilation -XX:CICompilerCount=$(nproc) -XX:ReservedCodeCacheSize=2G -XX:+DisableAttachMechanism -XX:-DontCompileHugeMethods -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UseFPUForSpilling -XX:+UseStringDeduplication -XX:+UseTransparentHugePages -XX:+UseXMMForArrayCopy -XX:+UseFastUnorderedTimeStamps"
+export JAVA_TOOL_OPTIONS="-XX:+AlwaysPreTouch -XX:CICompilerCount=$(nproc) -XX:ReservedCodeCacheSize=2G -XX:+DisableAttachMechanism -XX:-DontCompileHugeMethods -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UseFPUForSpilling -XX:+UseStringDeduplication -XX:+UseTransparentHugePages -XX:+UseXMMForArrayCopy"
 exec ./gradlew --no-daemon build
